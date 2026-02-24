665da4fbab1ab2e9b405ab21a3707106143490b8
JiangYancheng <98691384+jyczju@users.noreply.github.com>
2026-02-24 15:37:23 +0800

    fix

--- helperDownloadSLAMData.m
+++ helperDownloadSLAMData.m
@@ -13,7 +13,7 @@ function vioData = helperDownloadSLAMData()
 
     % vioData = load(fullfile(outputFolder,"BlackbirdVIOData","data.mat"));
 
-    % vioData = load("./BlackbirdVIOData/att_data_stable_f5c02_delay108.mat"); % 攻击数据
+    vioData = load("./BlackbirdVIOData/att_data_stable_f5c02_delay108.mat"); % 攻击数据
     % vioData = load("./BlackbirdVIOData/data_stable.mat");
-    vioData = load("./BlackbirdVIOData/data.mat"); % 原始数据
+    % vioData = load("./BlackbirdVIOData/data.mat"); % 原始数据
 end
